0e56b5be00ab6913a218e344e73c5964188cbb02
Hartwig Thomas <hartwig.thomas@enterag.ch>
2018-08-07 15:37:33 +0200

    .
    Signed-off-by: Hartwig Thomas <hartwig.thomas@enterag.ch>

--- src/ch/enterag/utils/StopWatch.java
+++ src/ch/enterag/utils/StopWatch.java
@@ -6,22 +6,6 @@ Description : StopWatch implements a utility for performance testing.
 ------------------------------------------------------------------------
 Copyright  : Enter AG, Zurich, Switzerland, 2010
 Created    : 30.08.2010, Hartwig Thomas
-------------------------------------------------------------------------
-The class ch.enterag.utils.StopWatch is free software; you can 
-redistribute it and/or modify it under the terms of version 2 of the 
-GNU General Public License as published by the Free Software Foundation.
-
-ch.enterag.utils.StopWatch is distributed in the hope that it will 
-be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
-of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-If you have a need for licensing ch.enterag.utils.StopWatch without 
-some of the restrictions specified in the GNU General Public License,
-it is possible to negotiate a different license with the copyright holder.
 ======================================================================*/
 package ch.enterag.utils;
 
