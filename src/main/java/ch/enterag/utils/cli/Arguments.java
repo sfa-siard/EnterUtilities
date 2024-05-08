5c5999e58e38e806faf18f5b06162bd58861c7f0
llorentelemmc <llorente@puzzle.ch>
2024-05-08 14:35:58 +0200

    refactor: Arguments, ArgumentsTest

--- src/main/java/ch/enterag/utils/cli/Arguments.java
+++ src/main/java/ch/enterag/utils/cli/Arguments.java
@@ -15,90 +15,114 @@ Created    : May 13, 2009, Hartwig Thomas
 ======================================================================*/
 package ch.enterag.utils.cli;
 
+import lombok.Getter;
+
 import java.io.*;
 import java.util.*;
 
-/*===================================================================*/
-/** This class parses the command line and makes the arguments
+/**
+ * This class parses the command line and makes the arguments
  * accessible as named options by name and unnamed arguments by
  * position.
- * In the name of simplicity this class does support switches that 
+ * In the name of simplicity this class does support switches that
  * indicate a boolean value just by their presence.
+ *
  * @author Hartwig Thomas
  */
-public class Arguments
-{
-  /** container of named options */
-  private Map<String,String> m_mapOptions = null;
-  /** get value of parsed option. 
-   * @param sName name of option 
-   * @return option value (null for missing option, "" for missing value) */
-  public String getOption(String sName) { return m_mapOptions.get(sName); }
-  /** container of unnamed arguments */
-  private String[] m_asArgument = null;
-  /** get value of unnamed argument. 
-   * @param iPosition position of argument (0 based) 
-   * @return argument value */
-  public String getArgument(int iPosition) { return m_asArgument[iPosition]; }
-  /** @return number of unnamed arguments. */
-  public int getArguments() { return m_asArgument.length; }
-  /** error string */
-  private String m_sError = null;
-  /** @return syntax error on command line. */
-  public String getError() { return m_sError; }
-  
-  /*------------------------------------------------------------------*/
-  /** constructor parses the command-line arguments
-   * @param args command-line arguments.
-   */
-  private Arguments(String[] args)
-  {
-    List<String> listArgument = new ArrayList<String>();
-    m_mapOptions = new HashMap<String,String>();
-    int iArgument = 0;
-    while (iArgument < args.length)
-    {
-      String sArgument = args[iArgument]; 
-      if (sArgument.startsWith("-") || (!File.separator.equals("/") && sArgument.startsWith("/")))
-      {
-        /* named argument: name is terminated by special character */
-        int iPosition = 1;
-        for (;
-          ((iPosition < sArgument.length() &&
-           Character.isLetterOrDigit(sArgument.charAt(iPosition))));
-          iPosition++) {}
-        if (iPosition > 1)
-        {
-          String sName = sArgument.substring(1,iPosition);
-          String sValue = "";
-          if (iPosition < sArgument.length())
-          {
-            if ((sArgument.charAt(iPosition) == ':') || 
-                (sArgument.charAt(iPosition) == '='))
-              sValue = sArgument.substring(iPosition+1); /* skip one ":", "=" */
-            else
-              m_sError = "Option " + sName + " must be terminated by colon, equals or blank!";
-          }
-          m_mapOptions.put(sName, sValue);
-        }
-        else
-          m_sError = "Empty option encountered!";
-      }
-      else
-        listArgument.add(args[iArgument]);
-      iArgument++;
-    }
-    m_asArgument = listArgument.toArray(new String[]{});
-  } /* constructor Arguments */
-  
-  /*------------------------------------------------------------------*/
-  /** factory parses the command-line arguments and returns the result.
-   * @param args command-line arguments.
-   * @return instance of Arguments.
-   */
-  public static Arguments newInstance(String[] args)
-  {
-    return new Arguments(args);
-  } /* newInstance */
+public class Arguments {
+    /**
+     * Container of named options.
+     */
+    private final Map<String, String> mapOptions;
 
-} /* class Arguments */
+    /**
+     * Container of unnamed arguments.
+     */
+    private String[] arguments = null;
+
+    @Getter
+    private String errorString = null;
+
+    /**
+     * Constructor parses the command-line arguments
+     *
+     * @param cliArguments command-line arguments.
+     */
+    public Arguments(String[] cliArguments) {
+        List<String> argumentList = new ArrayList<>();
+        mapOptions = new HashMap<>();
+        int i = 0;
+        while (i < cliArguments.length) {
+            String arg = cliArguments[i];
+            if (arg.startsWith("-") || (!File.separator.equals("/") && arg.startsWith("/")))
+                parseArguments(arg);
+            else
+                argumentList.add(cliArguments[i]);
+            i++;
+        }
+        arguments = argumentList.toArray(new String[]{});
+    }
+
+    /**
+     * Get value of parsed option.
+     *
+     * @param name Name of option.
+     * @return Option value (null for missing option, "" for missing value).
+     */
+    public String getOptions(String name) {
+        return mapOptions.get(name);
+    }
+
+    /**
+     * Get value of unnamed argument.
+     *
+     * @param argPosition Position of argument (0 based).
+     * @return Argument value.
+     */
+    public String getArgumentPosition(int argPosition) {
+        return arguments[argPosition];
+    }
+
+    /**
+     * @return Number of unnamed arguments.
+     */
+    public int getArgumentsNumber() {
+        return arguments.length;
+    }
+
+    /**
+     * Parses an argument to extract an option and its value.
+     *
+     * @param arg The command-line argument string to be parsed.
+     */
+    private void parseArguments(String arg) {
+        int optPosition = 1;
+        while (optPosition < arg.length() && Character.isLetterOrDigit(arg.charAt(optPosition))) {
+            optPosition++;
+        }
+        if (optPosition > 1) {
+            String optionName = arg.substring(1, optPosition);
+            String optionValue = "";
+            if (optPosition < arg.length()) {
+                if ((arg.charAt(optPosition) == ':') ||
+                        (arg.charAt(optPosition) == '='))
+                    optionValue = arg.substring(optPosition + 1);
+                else
+                    errorString = "Option " + optionName + " must be terminated by colon, equals or blank!";
+            }
+            mapOptions.put(optionName, optionValue);
+        } else
+            errorString = "Empty option encountered!";
+    }
+
+    /**
+     * @param args Command-line arguments.
+     * @return Arguments Instance.
+     * @deprecated Use {@link #Arguments} to create instances.
+     */
+    @Deprecated
+    public static Arguments getInstance(String[] args) {
+        return new Arguments(args);
+    }
+
+}
---
+++ src/test/java/ch/enterag/utils/cli/ArgumentsTest.java
@@ -0,0 +1,36 @@
+package ch.enterag.utils.cli;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgumentsTest {
+
+    private Arguments arguments;
+
+    @BeforeEach
+    void setUp() {
+        arguments = new Arguments(new String[]{"arg1", "-opt1=value1", "arg2", "-opt2"});
+    }
+
+    @Test
+    void shouldGetOptions() {
+        assertAll(
+                () -> assertEquals("value1", arguments.getOptions("opt1"), "Value of opt1 should be value1"),
+                () -> assertEquals("", arguments.getOptions("opt2"), "Value of opt2 should be set explicitly empty"),
+                () -> assertNull(arguments.getOptions("opt3"), "Non-existent option should return null")
+        );
+    }
+
+    @Test
+    void shouldGetArgumentPosition() {
+        assertEquals("arg1", arguments.getArgumentPosition(0), "First argument should be arg1");
+    }
+
+    @Test
+    void shouldGetArgumentsNumber() {
+        assertEquals(2, arguments.getArgumentsNumber(), "There should be two arguments");
+    }
+
+}
