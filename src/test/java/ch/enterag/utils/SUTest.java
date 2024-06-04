f3a9215fe9df18768e692b4a8d4c7d8efa9360f4
llorentelemmc <llorente@puzzle.ch>
2024-06-04 16:44:19 +0200

    refactor: SUTest

--- src/test/java/ch/enterag/utils/SUTest.java
+++ src/test/java/ch/enterag/utils/SUTest.java
@@ -1,18 +1,207 @@
 package ch.enterag.utils;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SUTest {
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
     @Test
-    void name() {
+    void isNotEmpty() {
         // given
 
         // when
-        String s = "refdgfg";
-        assertEquals(s.replace("g", "1"), SU.replace(s,"g","1"));
+
+        // then
+    }
+
+    @Test
+    void isNotWhite() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void replace() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void format() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void testFormat() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void repeat() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void testRepeat() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBreakPoint() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void putUtf8CharArray() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void putEncodedString() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void putUtf8String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void putCp437String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void putIsoLatin1String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void putWindows1252String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getEncodedString() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getUtf8String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getCp437String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getIsoLatin1String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getWindows1252String() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void toCsv() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void toHtml() {
+        // given
+
+        // when
+
         // then
     }
 }
