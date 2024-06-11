b06d1f5adb227106a68222c14d918cb220aa7419
llorentelemmc <llorente@puzzle.ch>
2024-06-11 15:49:44 +0200

    refactor: MimeTypes

--- src/main/java/ch/enterag/utils/mime/MimeTypes.java
+++ src/main/java/ch/enterag/utils/mime/MimeTypes.java
@@ -5,15 +5,19 @@ import org.apache.tika.mime.MimeTypeException;
 import static org.apache.tika.mime.MimeTypes.getDefaultMimeTypes;
 
 public class MimeTypes {
+    private static final String DEFAULT_EXTENSION = "bin";
 
-    public static String getExtension(String mimeType) throws MimeTypeException {
-        String extension = getDefaultMimeTypes().forName(mimeType).getExtension();
-        if (extension.isEmpty()) {
-            extension = "bin";
-        } else if (extension.startsWith(".")) {
-            extension = extension.substring(1);
+    public static String getExtension(String mimeType) {
+        try {
+            String extension = getDefaultMimeTypes().forName(mimeType).getExtension();
+            if (extension.startsWith(".")) {
+                return extension.substring(1);
+            } else {
+                return DEFAULT_EXTENSION;
+            }
+        } catch (MimeTypeException e) {
+            return DEFAULT_EXTENSION;
         }
-        return extension;
     }
 }
 
