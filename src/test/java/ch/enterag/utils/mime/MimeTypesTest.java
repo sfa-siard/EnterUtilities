671297e03c2a0e7cef0d49748b6b1b4e3bd989d9
llorentelemmc <llorente@puzzle.ch>
2024-06-11 08:41:56 +0200

    refactor: MimeTypes, MimeTypesTest

---
+++ src/test/java/ch/enterag/utils/mime/MimeTypesTest.java
@@ -0,0 +1,17 @@
+package ch.enterag.utils.mime;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MimeTypesTest {
+
+    @Test
+    void shouldGetExtension() {
+        assertAll(
+                () -> assertEquals("json", MimeTypes.getExtension("application/json")),
+                () -> assertEquals("png", MimeTypes.getExtension("image/png")),
+                () -> assertEquals("bin", MimeTypes.getExtension("invalid/mime-type"))
+        );
+    }
+}
